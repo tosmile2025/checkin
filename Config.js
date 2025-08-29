092ac59ac99c57ae49d5b695df955029fb53f618
tosmile <wajanachai.wo.budc@cpird.in.th>
2025-08-29 16:36:11 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,7 +1,7 @@
 // ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
-    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbwR5z1QhMd3588Hn5pW7kF55p0pUp4AG2ZmbzVDHTOhNBOFjjPuWdKIny8G8_rT0rodmA/exec';
-    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxu9h0ZJLm0h__BEahkGehV5nahKBs-aED6V4hYQnw-XxBmoUEuxIyZ4YHr_CRXEm53/exec';
+    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbycYAnTTkDa8mD2SgmiUvtOUwchGU7MNPYgiZ9YOLWwjBXTFpdSTMETu2TZCjVjj7-3/exec';
+    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbweRUbEmEy7s-7CVI3dgZz7wHgEPY7C-5VnhpNr3hFDWM0G3AzVVdQlaRuE4wiTtnZ0GQ/exec';
 //LIFF_ID
-    const LIFF_ID = '2007747842-OGeyrGkv'; // LIFF ID Checkin
-    const LIFF_HISTORY = '2007747842-p0YX1oBn'; // LIFF ID history
-    const LIFF_REGISTER = '2007747842-oJr1GXK2'; // LIFF ID register
+    const LIFF_ID = '2007475303-d5v41rEq'; // LIFF ID Checkin
+    const LIFF_HISTORY = '2007475303-JYyrOdWn'; // LIFF ID history
+    const LIFF_REGISTER = '2007475303-7wJz410Q'; // LIFF ID register
